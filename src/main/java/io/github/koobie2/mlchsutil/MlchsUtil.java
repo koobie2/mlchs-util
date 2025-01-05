a2b64fb48f87065b99e7ef8bf3853f23e3ffba37
koobie2 <rossrichards79@gmail.com>
2025-01-05 10:30:00 -0500

    inital commit

---
+++ src/main/java/io/github/koobie2/mlchsutil/MlchsUtil.java
@@ -0,0 +1,15 @@
+package io.github.koobie2.mlchsutil;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MlchsUtil extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
+	
+	@Override
+	public void 
+}
